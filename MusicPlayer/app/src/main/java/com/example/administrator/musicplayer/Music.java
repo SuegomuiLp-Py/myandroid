1848ad0e9f8953e0c76055c81aed9bbb89f6615c
SuegomuiLp-Py <buque198311@126.com>
2021-06-05 14:42:24 +0800

    MusicPlayerUI

---
+++ MusicPlayer/app/src/main/java/com/example/administrator/musicplayer/Music.java
@@ -0,0 +1,33 @@
+package com.example.administrator.musicplayer;
+
+public class Music {
+
+    private String musicName;
+    private String musicArtist;
+    private String musicPath;
+    private String musicDuration;
+
+    public Music (String musicName,String musicArtist,String musicPath,String musicDuration)
+    {
+        this.musicName = musicName;
+        this.musicArtist = musicArtist;
+        this.musicPath = musicPath;
+        this.musicDuration = musicDuration;
+    }
+    public String getmusicName()
+    {
+        return this.musicName;
+    }
+    public String getmusicArtist()
+    {
+        return this.musicArtist;
+    }
+    public String getmusicPath()
+    {
+        return this.musicPath;
+    }
+    public String getmusicDuration()
+    {
+        return this.musicDuration;
+    }
+}
